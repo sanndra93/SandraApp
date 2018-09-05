d5efc869a031ee2c879933d0f128f3899aca51e3
Sandra <sandracorlovic@gmail.com>
2018-09-05 11:05:36 +0200

    Implement listing articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
 	validates :title, presence: true
 	validates :body, presence: true
+
+	#prikaz poslednjeg(najnovijeg) napravljenog artikla na vrhu
+	default_scope { order(created_at: :desc)}
 end
